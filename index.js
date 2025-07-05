07313645be05a4b0d5465d780ec8da188192efd3
git config --global user.name <mostafakhairy200@gmail.com>
2025-07-05 06:18:51 +0300

    slight commit

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const PORT = process.env.PORT || 3500;
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+//routes
 app.use('/api/register' , require('./routes/api/registerRoute'));
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- validators/user_validator.js
+++ validators/user_validator.js
@@ -4,6 +4,9 @@ const registerValidator = [
   body('name').notEmpty().withMessage('Name is required'),
   body('email').isEmail().withMessage('Valid email is required'),
   body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 chars'),
+  body('passwordConfirmation').custom((value, { req }) => {
+    return value === req.body.password;
+  }).withMessage('Passwords do not match'),
 ];
 
 module.exports = { registerValidator };
